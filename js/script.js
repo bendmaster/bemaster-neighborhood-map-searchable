f6eee44654525b1516d3f8cd0d14202126ed607b
bendmaster <mastersbd@gmail.com>
2017-01-13 18:52:54 -0800

    buggy as hell but works

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var map;
-
+var locations = [];
+var markers = [];
+var foursquare = '';
 
 //Initialize the map and its contents
 function initMap() {
@@ -66,7 +68,24 @@ function initMap() {
 
     infowindow = new google.maps.InfoWindow();
 
-    setMarkers(locations);
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: greenlake,
+        radius: 1000,
+        type: ['bar']
+    }, callback);
+
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < 10; i++) {
+                var place = results[i];
+                addLocation(place);
+                createMarker(place);
+            }
+        }
+    }
+
+
 
     setAllMap();
 
@@ -77,84 +96,122 @@ function initMap() {
 function setAllMap() {
   for (var i = 0; i < locations.length; i++) {
     if(locations[i].boolTest === true) {
-    locations[i].holdMarker.setMap(map);
+        markers[i].setVisible(true);
     } else {
-    locations[i].holdMarker.setMap(null);
+        markers[i].setVisible(false);
     }
   }
 }
 
+
+
+function addLocation(place) {
+
+    var location = {};
+    location.place_id = place.place_id;
+    location.position = place.geometry.location.toString();
+    location.name = place.name;
+    location.vicinity = place.vicinity;
+    location.rating = place.rating;
+    location.visible = ko.observable(true);
+    location.boolTest = true;
+    locations.push(location);
+    console.log("pushed: " + location);
+}
+
 //Information about the different locations
 //Provides information for the locations
-var locations = [
-    {
-    name: "The Thomas Jefferson Memorial",
-    lat: 47.680433,
-    lng: -122.325661,
-    // id: "nav0",
-    visible: ko.observable(true),
-    boolTest: true
-    }
-];
 
 
+function createMarker(place) {
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        name: place.name,
+        place_id: place.place_id,
+        animation: google.maps.Animation.DROP,
+        vicinity: place.vicinity,
+        rating: place.rating
+    });
+
+    //when marker is clicked on the map, the clickMarker function is run
+    google.maps.event.addListener(marker, 'click', function () {
+        clickMarker(place);
+    });
+    markers.push(marker);
+    return marker;
 
 
-function setMarkers(location) {
-
-    for(i=0; i<location.length; i++) {
-        location[i].holdMarker = new google.maps.Marker({
-          position: new google.maps.LatLng(location[i].lat, location[i].lng),
-          map: map,
-          name: location[i].name
-        });
-
-        
-
-        //Binds infoWindow content to each marker
-        location[i].contentString = '<hr style="margin-bottom: 5px"><strong>' +
-                                    location[i].name + '</strong>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: locations[i].contentString
-        });
-
-        //Click marker to view infoWindow
-            //zoom in and center location on click
-        new google.maps.event.addListener(location[i].holdMarker, 'click', (function(marker, i) {
-          return function() {
-            infowindow.setContent(location[i].contentString);
-            infowindow.open(map,this);
-            var windowWidth = $(window).width();
-            if(windowWidth <= 1080) {
-                map.setZoom(14);
-            } else if(windowWidth > 1080) {
-                map.setZoom(16);
-            }
-            map.setCenter(marker.getPosition());
-            location[i].picBoolTest = true;
-          };
-        })(location[i].holdMarker, i));
-
-        //Click nav element to view infoWindow
-            //zoom in and center location on click
-        // var searchNav = $('#nav' + i);
-        // searchNav.click((function(marker, i) {
-        //   return function() {
-        //     infowindow.setContent(location[i].contentString);
-        //     infowindow.open(map,marker);
-        //     map.setZoom(16);
-        //     map.setCenter(marker.getPosition());
-        //     location[i].picBoolTest = true;
-        //   };
-        // })(location[i].holdMarker, i));
-    }
 }
+
+function clickMarker(place) {
+    getFoursquare(place);
+    console.log("Clicking...")
+
+    //this timeout function gives the foursquare API a bit of time to retrieve the JSON data
+
+    setTimeout(function () {
+        var marker;
+
+        for (var i = 0; i < markers.length; i++) {
+            if (place.place_id === markers[i].place_id) {
+                marker = markers[i];
+            }
+        }
+        var content = "<div><b>" + place.name + "</b></div><br><div>" + place.vicinity + "</div><br><div>Rating: " + place.rating + "</div><br>" + foursquare;
+        infowindow.setContent(content);
+        infowindow.open(map, marker);
+
+        map.panTo(marker.position);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function () {
+            marker.setAnimation(null);
+        }, 1450);
+    }, 300);
+}
+
+function getFoursquare(point) {
+    // foursquare api url
+    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?client_id=NONCPLRWZOOGK1CXCJUTJM2J5KWAXNW1F2135GWMLHQIL2LB&client_secret=5GYN3U3I0TM3JLPDB5P4DQW2X0NZNJCL1O2AXTFIJ0Y2ICE5&v=20170111&ll=47.680677,-122.323503&query=\'' + point['name'] + '\'&limit=10';
+
+    // retrievies the JSON from the foursquare URL and provides the phone number and twitter account, if available of each location
+    $.getJSON(foursquareURL).done(function (response) {
+        foursquare = '';
+        var venue = response.response.venues[0];
+
+        //functions normally if data is retrieved about the location but, if no data is available, runs the else statement
+        if (venue) {
+            var phone = venue.contact.formattedPhone;
+            if (phone !== undefined) {
+                foursquare += 'Phone: ' + phone + '<br>';
+            } else {
+                foursquare += 'Phone: Not Available' + '<br> ';
+            }
+
+            var twitter = venue.contact.twitter;
+            if (twitter !== undefined) {
+                foursquare += '<br>' + 'Twitter: @' + twitter;
+            }
+            else {
+                foursquare += '<br>' + 'Twitter: Not Available' + ' ';
+            }
+        }
+        else {
+            foursquare += 'Foursquare unavailable';
+        }
+
+        //alerts user of error if foursquare API fails
+    }).fail(function (e) {
+        alert('Foursquare API is not working. Error: ' + e);
+    });
+}
+
 
 //Query through the different locations from nav bar with knockout.js
     //only display locations and nav elements that match query result
 var viewModel = {
-    query: ko.observable(''),
+    query: ko.observable('')
 };
 
 viewModel.locations = ko.dependentObservable(function() {
